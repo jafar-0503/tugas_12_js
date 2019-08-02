a060b9fa87c5af7cc041e8f339042be1138949b3
Mukhamat Jafar <jafar.mukhamat7@gmail.com>
2019-08-02 22:35:28 +0700

    tugas_12_js
    tugas sudah ok !!!

---
+++ tugas-12.js
@@ -0,0 +1,14 @@
+function panggilbuah(){
+
+  var buah = ["Pisang", "Jeruk", "Apel", "Mangga"];
+  console.log(buah);
+  console.log("");
+  buah.pop();
+  console.log(buah);
+  console.log("");
+  buah.shift();
+  
+  return buah;
+
+}
+console.log(panggilbuah());
